46612748924cf4dca6d76441fa78f568f480ec11
EDC M1PRO <cadiz.edward@gmail.com>
2022-05-25 19:31:17 -0700

    created components and styles folders to hold new components and css in case, added oktokit to grab github commit info, imported useState and useEffect for use with api calls with octokit.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Octokit } from '@octokit/core';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [commitData, setCommitData] = useState({})
+
+  useEffect(() => {
+
+    const octokit = new Octokit({
+      auth: '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'
+    })
+
+    const getCommits = async () => {
+      try {
+        return await octokit.request('GET /repos/{owner}/{repo}/commits', {
+          owner: 'userFame',
+          repo: 'crossroads-take-home-test'
+        })
+      } catch (err) {
+        throw err
+      }
+    }
+
+    getCommits().then(res => {setCommitData(res); console.log(res)}).catch(err => console.log(err, 'error'))
+
+  }, [])
+
   return (
     <div className="App">
-
+      
     </div>
   );
 }
